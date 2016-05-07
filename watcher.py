035b58aedd40a076b0ea75540474cb6aae6c68c4
Nicci00 <nicci@shijou.moe>
2016-05-07 04:11:48 -0300

    TuneIn AirAPI integration :radio:

--- watcher.py
+++ watcher.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-from sys import stdout
 from time import sleep
 from ConfigParser import SafeConfigParser
 import xml.etree.ElementTree as ET
+import sys
 import json
 import requests
+import threading
 
 parser = SafeConfigParser()
 parser.read('config.ini')
+
+data = None
 
 def getTree():
 	ice_user = parser.get('server', 'icecast_username')
@@ -18,15 +21,14 @@ def getTree():
 	get = requests.get(ice_url, auth=(ice_user, ice_pass))
 
 	if get.status_code != 200:
-		raise Exception("status code %s" % get.status_code)
+		raise Exception("Icecast Server status code %s" % get.status_code)
 	else:
 		return ET.ElementTree(ET.fromstring(get.text))
 
+
 def getData():
-
 	xmlTree = getTree()
-
-	nodes = getTree().findall(".//listeners")
+	nodes = xmlTree.findall(".//listeners")
 
 	return {
 		"artist" : unicode(xmlTree.find(".//artist").text),
@@ -34,22 +36,47 @@ def getData():
 		"listeners" : int(nodes[1].text) + int(nodes[2].text)
 	}
 
-data = getData()
 
 def send_data():
 	print(json.dumps(data))
+	sys.stdout.flush()
 
-	stdout.flush()
 
-send_data()
+def send_tunein_data():
+	req = requests.get("http://air.radiotime.com/Playing.ashx?partnerId={0}&partnerKey={1}&id={2}&title{3}&artist={4}".format(
+			parser.get('tunein', 'partnerId'),
+			parser.get('tunein', 'partnerKey'),
+			parser.get('tunein', 'radioId'),
+			data['title'].encode('utf-8'),
+			data['artist'].encode('utf-8')))
 
-while True:
+	if req.status_code != 200:
+		raise Exception("TuneIn Air API request status code %s" % get.status_code)
 
-	sleep(2)
-	rfData = getData()
 
-	if rfData != data:
+def watcher():
+	global data
+	global debug
 
-		data = rfData
-		sleep(1)
-		send_data()
+	while True:
+		sleep(2)
+		rData = getData()
+
+		if rData['title'] != data['title'] and rData['artist'] != data['artist']:
+			data = rData
+			send_data()
+			send_tunein_data()
+
+
+if __name__ == '__main__':
+
+	debug = sys.argv[1] == '--debug'
+
+	data = getData()
+
+	send_data()
+	send_tunein_data()
+
+	thread = threading.Thread(target = watcher)
+	thread.start()
+	thread.join()
