e4c4cbaf4bd50ea7f7308a22c0e6894172ae0ff3
Nicci00 <nicci@shijou.moe>
2016-07-18 20:05:00 -0400

    new feature: slugify title and rename files with it

--- renamer.py
+++ renamer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python2
+
+from slugify import slugify
+from mutagen.easyid3 import EasyID3
  
 import os
 import sys
@@ -9,15 +12,22 @@ if len(sys.argv) == 1:
 	exit(1)
  
 music_folder = sys.argv[1]
+
+files = filter(lambda x: x.endswith('.mp3'), os.listdir(music_folder))
  
-for file in os.listdir(music_folder):
- 
-	print 'old name: ' + file
-	file_path = music_folder + file
+for file in files:
+	file_path = music_folder + file.decode('utf-8')
+
+	id3 = EasyID3(file_path)
+
+	slug = slugify(unicode(id3['title'][0]))
 	rn = str(randint(11,99))
-	new_name = rn + file[2:]
- 
+
+	print 'Old name: ' + file
+
+	new_name = "{}_{}.mp3".format(str(rn), slug)
 	os.rename(file_path, music_folder + new_name)
-	print 'new name: ' + new_name + '\n'
+
+	print 'New name: ' + new_name + '\n'
  
 exit(0)
